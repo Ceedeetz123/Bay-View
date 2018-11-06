08aa9c1cc7bb7e5a0763d53226f6a42670bdb0d9
Chi Hoo Tsang <17144159@students.southwales.ac.uk>
2018-11-06 21:55:32 +0000

    Login Rough Prototype

--- Bay View/Bay View/Form1.cs
+++ Bay View/Bay View/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Bay_View
 {
@@ -16,5 +17,39 @@ namespace Bay_View
         {
             InitializeComponent();
         }
+        SQLiteConnection Conn = new SQLiteConnection(dbConns.Source);
+        //Connection Object for linking the database
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection Conn = new SQLiteConnection())
+            {
+                Conn.ConnectionString = dbConns.Source;
+                string sql = "SELECT Password, FROM staff WHERE Staff_ID=@Username,";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, Conn))
+                {
+                    cmd.Parameters.AddWithValue("Username", tbUsername);
+                    Conn.Open();
+                    using (SQLiteDataReader DataRead = cmd.ExecuteReader())
+                    {
+                        if (!DataRead.HasRows)
+                            throw new Exception();
+                        //When the password is not correct according to the Database
+                        DataRead.Read();
+                        if (tbPassword.Text != DataRead[0].ToString())
+                            throw new Exception();
+                        //If username and Password match the record in the database
+                        string Message = "Successfull Login";
+                        MessageBox.Show(Message);
+                        Conn.Close();
+
+
+                    }
+
+                }
+            }
+
+        }
+
     }
 }
+
---
+++ Bay View/Bay View/dbConns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bay_View
+{
+    class dbConns
+    {
+        public static string
+        Source = @"Data Source=Bay-View.db";
+    }
+}
